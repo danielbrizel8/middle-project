fce4d416f23d1787bf81ebb62452a57b09727a82
danielbrizel8 <daniel.brizel@gmail.com>
2023-05-31 15:40:34 +0300

    rs conflicts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import '../styling/layout.css'
+import Logo from './../assets/logo.png'
+
 
 function Layout() {
     return (
         <div>
             <nav className='nav-bar'>
-                <Link className='logo' to={"/"}></Link>
+               <Link className='logo' to={"/"}><img className='image-logo' src={Logo} alt="" /> </Link>
                 <Link className='link' to={"/LogIn"}>Log-in</Link>
                 <a className='link' href="#about-us-section">About- us</a>
                 <a className='link' href="#available-dates-section">Available Dates</a>
