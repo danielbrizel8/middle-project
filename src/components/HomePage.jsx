2a575f8fab964cb55e4ab488142d225c3e0948f1
danielbrizel8 <daniel.brizel@gmail.com>
2023-05-31 10:25:35 +0300

    home page styling

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -13,10 +13,10 @@ function HomePage() {
       </div>
       <div className="time-line-container">
         <div className='time-line-title'>
-          <h1>Create Your Perfect Wedding Event</h1>
-          <h2>Effortlessly plan, customize, and bring your dream wedding to life with our intuitive app.</h2>
+          <h1>So where do we start?</h1>
+          <h2>Let us sort out all the mess for you with the unique platform that will accompany you step by step.</h2>
           <div>
-            <img src="https://easy2give.co.il/media/images/family-icon.original.png" alt="" />
+            <img className='image-couple' src="https://easy2give.co.il/media/images/family-icon.original.png" alt="" />
           </div>
         </div>
         <div className='time-line'>
@@ -24,13 +24,13 @@ function HomePage() {
             <div className="time-line-box">
               <div className="box-details">
                 <div className="image-time-line">
-                  <img className="image-time-line" src="https://oionline.com/wp-content/uploads/2017/01/february-14-calendar-valentines-day.jpeg" alt="" />
+                  <img className="image-time-line" src="https://www.brides.com/thmb/YkPH2kwcnAlO7n6OslSrkOzMDck=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/calendar-eded7d6cce89406fa85aff003a4899ed.jpg" alt="" />
                 </div>
                 <div className="content-time-line">
-                  <h1 className='content-title'>Pick a Date</h1>
+                  <h1 className='content-title'>Pick Your Date</h1>
                   <h3 className='content-paragraph'>Selecting your dream wedding date is made easy with our interactive tools. Explore venue availability, consider seasonal factors, and ensure a conflict-free celebration. </h3>
                   <div className='button-link'>
-                  <Link to={'Calendar'}>Date</Link>
+                    <Link className='link-more' to={'Calendar'}>More</Link>
                   </div>
                 </div>
               </div>
@@ -42,9 +42,9 @@ function HomePage() {
                 </div>
                 <div className="content-time-line">
                   <h1 className='content-title'>Let's Get Married</h1>
-                  <h3 className='content-paragraph'>Selecting your dream wedding date is made easy with our interactive tools. Explore venue availability, consider seasonal factors, and ensure a conflict-free celebration. </h3>
+                  <h3 className='content-paragraph'>Start your journey to a perfect wedding day with us. Our seamless planning tools and expert team make it effortless to bring your dream wedding to life. From venue selection to personalized details, let us guide you every step of the way towards a magical celebration of love at [Wedding Hall Name]. </h3>
                   <div className='button-link'>
-                    <Link to={'CreateEvent'}>Event</Link>
+                    <Link className='link-more' to={'CreateEvent'}>More</Link>
                   </div>
                 </div>
               </div>
@@ -52,15 +52,15 @@ function HomePage() {
             <div className="time-line-box">
               <div className="box-details">
                 <div className="image-time-line">
-                <div className="image-time-line">
-                  <img className="image-time-line" src='https://images.ctfassets.net/wzm83lc7g93q/5HrYApoPBfKPbw0vHdRDmB/b9e19496fa3b91c88285c19fecfbc6b2/getting_married_in_british_columbia-high.jpg' alt="" />
-                </div>
+                  <div className="image-time-line">
+                    <img className="image-time-line" src='https://www.womangettingmarried.com/wp-content/uploads/2023/01/wedding-colors-on-table.jpg' alt="" />
+                  </div>
                 </div>
                 <div className="content-time-line">
-                  <h1 className='content-title'>Our deals</h1>
-                  <h3 className='content-paragraph'>Selecting your dream wedding date is made easy with our interactive tools. Explore venue availability, consider seasonal factors, and ensure a conflict-free celebration. </h3>
+                  <h1 className='content-title'>Our Deals</h1>
+                  <h3 className='content-paragraph'>Discover extraordinary wedding deals at [Wedding Hall Name]. From all-inclusive packages to customizable options, our exclusive offers cater to your unique preferences and budget. Choose [Wedding Hall Name] for Createing unforgettable memories while enjoying exceptional value. </h3>
                   <div className='button-link'>
-                  <Link to={'Packages'}>deals</Link>
+                    <Link className='link-more' to={'Packages'}>More</Link>
                   </div>
                 </div>
               </div>
@@ -68,21 +68,33 @@ function HomePage() {
             <div className="time-line-box right-pair">
               <div className="box-details">
                 <div className="image-time-line">
-                <div className="image-time-line">
-                  <img className="image-time-line" src='https://i.etsystatic.com/32703807/r/il/99b8f3/3493484389/il_1080xN.3493484389_m57u.jpg' alt="" />
-                </div>
+                  <div className="image-time-line">
+                    <img className="image-time-line" src='https://i.etsystatic.com/32703807/r/il/99b8f3/3493484389/il_1080xN.3493484389_m57u.jpg' alt="" />
+                  </div>
                 </div>
                 <div className="content-time-line">
                   <h1 className='content-title'>Send Inviation</h1>
-                  <h3 className='content-paragraph'>Selecting your dream wedding date is made easy with our interactive tools. Explore venue availability, consider seasonal factors, and ensure a conflict-free celebration. </h3>
+                  <h3 className='content-paragraph'>Join us and you can see how your wedding invitation will look like. </h3>
                   <div className='button-link'>
-                  <Link to={'Inviation'}>test</Link>
+                    <Link className='link-more' to={'Inviation'}>More</Link>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <div>
+          <img className='image-couple' src="https://easy2give.co.il/media/images/married-icon.original.png" alt="" />
+        </div>
+        <div>
+          <div className='about-us-section'>
+            <h1>About- us</h1>
+          </div>
+          <div className='about-us-content'>
+            <h2>Welcome to [Wedding Hall Name], where unforgettable memories are made. With [number] years of experience, we are dedicated to providing an exceptional wedding experience. Our elegant venue, personalized service, and attention to detail ensure your special day is truly magical. Let us be part of your love story and create an extraordinary celebration at [Wedding Hall Name].</h2>
+          </div>
+        </div>
+
       </div>
     </div>
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,12 +8,11 @@ function Layout() {
             <nav className='nav-bar'>
                 <Link className='logo' to={"/"}></Link>
                 <Link className='link' to={"/LogIn"}>Log-in</Link>
-                <Link className='link' to={"/Gift"}>Online Gift</Link>
-                <a className='link' href="#about-us-section">About- us</a> 
+                <a className='link' href="#about-us-section">About- us</a>
                 <a className='link' href="#available-dates-section">Available Dates</a>
-                <a className='link' href="#our-packages-section">Our Packages</a>
                 <a className='link' href="#create-event-section">Create Event</a>
-                
+                <a className='link' href="#our-packages-section">Our Deals</a>
+                <Link className='link' to={"/Gift"}>Online Gift</Link>
             </nav>
             <Outlet />
         </div>
