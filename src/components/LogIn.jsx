f13aad7797adff04f1dde3029dc6f7ad3a5b9eef
amitbarha <amitbar131@gmail.com>
2023-06-01 08:44:53 +0300

    new log in

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './../styling/log-in.css'
 import { useForm } from "react-hook-form";
+import { useNavigate } from 'react-router';
 import { TextField } from '@mui/material';
+import users from './../usersData.json'
+
 
 function LogIn() {
+  const navigate = useNavigate()
+  console.log(users);
+  const [usersData, setUsersData] = useState()
+  useEffect(() => {
+    setUsersData(users.users)
+}, [])
+  const [userName, setUserName] = useState()
+  const [password, setPassword] = useState()
+  const handleCheck = () =>
+  {
+    const checkUser = usersData?.find((user) => user.userName == userName)
+    if (checkUser != undefined) {
+      if (checkUser.password == password) {
+        console.log("yeah");
+        navigate('/HomePage')
+      }
+    }
+    else{
+      console.log(no);
+    }
+    console.log(checkUser);
+  }
+
 
   return (
-    <div>
-      <TextField label="User Name"/>
-      <TextField label="password"/>
+    <div className='login-container'>
+      <div className="log-in-box">
+      <div className="title-login">
+        <h1>Gan Eden</h1>
+        <h2>Unlock the Gates to Your Dream Wedding</h2>
+      </div>
+      <div className="inputs-login">
+      <TextField className='login-input' label="User Name" onChange={(e) => setUserName(e.target.value)}/>
+      <TextField className='login-input' label="password" onChange={(e) => setPassword(e.target.value)}/>
+      </div>
+      <div className="button-login">
+      <button className='sign-in-button' onClick={handleCheck}>Log In</button>
+      </div>
+      </div>
     </div>
   )
 }
